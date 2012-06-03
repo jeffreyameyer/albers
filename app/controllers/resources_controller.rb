20c8c568d98636fd718755f3444fdcd651b03aa9
Christopher Kruse <kruse.christopher@gmail.com>
2012-06-03 14:30:06 -0700

    delayed job when no page id

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -15,6 +15,16 @@ class ResourcesController < ApplicationController
   def update
     @resource = Resource.find(params[:id])
     # params[:downvote] or params[:upvote]
+    tags = JSON.parse(params[:tags]) rescue []
+    unless tags.blank?
+      if params[:upvote]
+
+      elsif params[:downvote]
+
+      end
+    end
+    @resource.save!
+    respond_with @resource
   end
 
   def search
@@ -22,7 +32,7 @@ class ResourcesController < ApplicationController
     page = (params[:page].blank? ? 1 : params[:page])
     @resources = Resource.solr_search { fulltext query; paginate :page => page}.results
     logger.warn "RESOURCES: #{@resources}"
-    if params[:page] && params[:page].to_i <= 1
+    if params[:page].blank? || params[:page].to_i <= 1
       if @resources.count < Resource.threshold
         Delayed::Job.enqueue(CollectResourceJob.new(query))
       end
