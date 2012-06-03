a2c0cfe5e06c82f0a22fbb16c9043fa8b704d8da
Christopher Kruse <kruse.christopher@gmail.com>
2012-06-03 16:00:43 -0700

    resource fix

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,14 +4,13 @@ class Resource
   field :uri
   field :visible_uri
   has_many :comments
-  field :tags_array
-
+  field :tags_array, :default => []
   after_save :reindex
 
   include Sunspot::Mongo
   searchable do
     text :tag do
-      self.tags_array.map{|t| t['tag_name']}.join(", ")
+      tags_array.map{|t| t['tag_name']}.join(", ")
     end
     text :uri
     text :title
