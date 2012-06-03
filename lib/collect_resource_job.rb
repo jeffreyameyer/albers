bbfb6082ee070133ff25dec9e98a3435d32fe4d3
Christopher Kruse <kruse.christopher@gmail.com>
2012-06-03 13:50:36 -0700

    tags array still

--- lib/collect_resource_job.rb
+++ lib/collect_resource_job.rb
@@ -2,15 +2,17 @@ class CollectResourceJob  < Struct.new(:query)
   def perform
     results = JSON.parse(ImageSearch.run(query))
     tags = TokenizeQuery.run(query)
-    tags.map do |tag|
+    built_tags = []
+    tags.each do |tag|
       t = Tag.find_or_create_by(:name => tag)
       t.save
+      built_tags << t
     end
 
     results.each do |result|
       res = Resource.new(result)
       res.tags_array = []
-      tags.each do |tag|
+      built_tags.each do |tag|
         res.tags_array << {:tag_id => tag._id, :rating => 0}
       end
       res.save
