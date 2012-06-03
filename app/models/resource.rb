84e50938ffb1571bf95608d26a4913e49b4391e8
Christopher Kruse <kruse.christopher@gmail.com>
2012-06-03 15:54:29 -0700

    change default, futz with indexing'

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -10,10 +10,11 @@ class Resource
 
   include Sunspot::Mongo
   searchable do
-    string :tag, :multiple => true do
-      self.tags_array.map{|t| t['tag_name']}
+    text :tag do
+      self.tags_array.map{|t| t['tag_name']}.join(", ")
     end
     text :uri
+    text :title
   end
 
   THRESHOLD = 20 #Number of resources needed to not kick off job
